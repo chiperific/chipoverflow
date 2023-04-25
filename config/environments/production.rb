0b17b7117e8a45b8cca50dc8d45dbf6f48f11fa7
Chip <chiperific@gmail.com>
2023-04-24 20:19:24 -0400

    list domains
    Git Haiku 🌸
    Domains all in sight,
    A list for the world to see,
    Internet mapped out.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,9 @@ Rails.application.configure do
     password: ENV.fetch('SENDGRID_API_KEY', nil)
   }
 
-  config.hosts << ['chipoverflow.up.railway.app', 'chipoverflow.com']
+  ['chipoverflow.up.railway.app', 'chipoverflow.com'].each do |domain|
+    config.hosts << domain
+  end
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
